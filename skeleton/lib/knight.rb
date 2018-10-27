2be689b65dbb8dc3f8c70285dfec044b78deff53
Safuh95 <safoh95@windowslive.com>
2018-10-26 18:30:25 -0700

    Happy Friday

---
+++ skeleton/lib/knight.rb
@@ -0,0 +1,13 @@
+require_relative "00_tree_node"
+
+class KnightPathFinder
+  attr_reader :init_pos
+  
+  def initialize(pos=[0,0])
+    @init_pos = pos
+  end
+  
+  def find_path(pos)
+    
+  end
+end 
